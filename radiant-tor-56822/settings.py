6d0db1263b65465ff2388ec8800b21cc791bb936
roleeeek <lidvinskiy@gmail.com>
2016-11-22 18:22:56 +0200

    'v0.2'

--- radiant-tor-56822/settings.py
+++ radiant-tor-56822/settings.py
@@ -118,7 +118,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'static/css')
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
