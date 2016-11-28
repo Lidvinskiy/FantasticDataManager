9589ed94947fb4d126a3ff6030fa787f1375098c
roleeeek <lidvinskiy@gmail.com>
2016-11-28 16:10:15 +0200

    'com'

--- worker.py
+++ worker.py
@@ -5,15 +5,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('redistogo-spherical-73382','redis://redistogo:897d4c29fb07e5d500bb38d2c26c64f1@crestfish.redistogo.com:9158/')
-url = urlparse.uses_netloc(redis_url)
+#redis_url = os.getenv('redistogo-spherical-73382','redis://redistogo:897d4c29fb07e5d500bb38d2c26c64f1@crestfish.redistogo.com:9158/')
+url = urlparse.urlparse(os.environ.get('redistogo-spherical-73382','redis://redistogo:897d4c29fb07e5d500bb38d2c26c64f1@crestfish.redistogo.com:9158/'))
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
-
-
-def get_conn():
-    return conn
-
-
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
