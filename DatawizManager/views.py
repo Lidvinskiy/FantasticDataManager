89e6ba37a2385aa0d67957302e4dec565eb74f79
roleeeek <lidvinskiy@gmail.com>
2016-11-29 21:51:19 +0200

    'final'

--- DatawizManager/views.py
+++ DatawizManager/views.py
@@ -100,6 +100,7 @@ def ping_for_queue(request, shops='', date_from_first='', date_to_first='', date
     key = str(request.GET['type'].encode('utf-8')) + str(date_from_f) + str(date_to_f) + \
           str(date_from_s) + str(date_to_s) \
           + str(shops_int) + str(request.session['login'])
+    # отримуемо результат завдання з черги по id
     if str(request.GET['type'].encode('utf-8')) == 'get_data':
         job = q.fetch_job(request.session['get_base_q_id'])
     elif str(request.GET['type'].encode('utf-8')) == 'change_inform':
@@ -124,6 +125,7 @@ def get_base_data_to_html(request, shops='', date_from_first='', date_to_first='
           str(date_from_s) + str(date_to_s) \
           + str(shops_int) + str(request.session['login'])
     if cache.get(key) is None:
+        # додаємо завдання до черги
         job = q.enqueue(
             BAL_create_base_inform, QueueBase(request.session['login'], request.session['key'], shops_int, date_from_f,
                                               date_to_f,
@@ -167,6 +169,7 @@ def change_inform(request, shops='', date_from_first='', date_to_first='', date_
           str(date_from_s) + str(date_to_s) \
           + str(shops_int) + str(request.session['login'])
     if cache.get(key) is None:
+        # додаємо завдання до черги
         job = q.enqueue(
             BAL_create_change_inform,
             QueueBase(request.session['login'], request.session['key'], shops_int, date_from_f,
--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 import urlparse
 from redis import Redis
@@ -5,7 +6,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-#redis_url = os.getenv('redistogo-spherical-73382','redis://redistogo:897d4c29fb07e5d500bb38d2c26c64f1@crestfish.redistogo.com:9158/')
+# Налаштування для черги виконання завдань
 url = urlparse.urlparse(os.environ.get('redistogo-spherical-73382','redis://redistogo:897d4c29fb07e5d500bb38d2c26c64f1@crestfish.redistogo.com:9158/'))
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 if __name__ == '__main__':
