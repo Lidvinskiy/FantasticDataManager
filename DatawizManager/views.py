db0aff60b19900a241b07ff0453a20a3f422e7fb
roleeeek <lidvinskiy@gmail.com>
2016-11-29 15:58:24 +0200

    'com'

--- DatawizManager/views.py
+++ DatawizManager/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.cache import cache
 from dwapi import datawiz
-from rq import Queue, get_current_job
+from rq import Queue, get_current_job, Job
 from worker import conn
 import pandas as pd
 import datetime
@@ -78,6 +78,7 @@ def BAL_create_base_inform(getinform):
                                    getinform.date_to_f,
                                    getinform.date_from_s, getinform.date_to_s).base_information_table.to_html(
         classes=['table', 'table-striped', 'table-hover', 'table-responsive'], border=0)
+
     # cache.set(getinform.key_to_cache, query)
     return query
 
@@ -93,13 +94,14 @@ def ping_for_queue(request, shops='', date_from_first='', date_to_first='', date
           str(date_from_s) + str(date_to_s) \
           + str(shops_int) + str(request.session['login'])
     # print get_current_job()
-    print q.fetch_job(key)
-    print q.fetch_job(key).return_value
-    if not q.fetch_job(key).is_finished:
+    job = Job.fetch(key, conn)
+    print job
+    print job.return_value
+    if not job.is_finished:
         return HttpResponse('')
     else:
-        cache.set(key, q.fetch_job(key).return_value)
-        return HttpResponse(q.fetch_job(key).return_value)
+        cache.set(key, job.return_value)
+        return HttpResponse(job.return_value)
 
 
 def get_base_data_to_html(request, shops='', date_from_first='', date_to_first='', date_from_second='',
